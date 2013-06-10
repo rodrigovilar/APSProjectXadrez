a357947e1c59709eec6c8d82e84d43efcc478231
hugomoreira <hugomoreira@dce.ufpb.br>
2013-06-10 09:39:20 -0300

    Colocando Cenario 5 no lugar correto.

--- src/xadrez/Teste.java
+++ src/xadrez/Teste.java
@@ -39,6 +39,24 @@ public class Teste {
 
 		return jogo;
 	}
+	
+	public JogoXadrez cenario5() {
+		JogoXadrez jogo = new JogoXadrez();
+		// movimento do peao branca
+		assertTrue(jogo.movimentarPeca(1, 0, 2, 0));
+		assertTrue(jogo.movimentarPeca(2, 0, 3, 0));
+		// movimento da torre branca
+		assertTrue(jogo.movimentarPeca(0, 0, 1, 0));
+		assertTrue(jogo.movimentarPeca(1, 0, 2, 0));
+		assertTrue(jogo.movimentarPeca(2, 0, 2, 1));
+		assertTrue(jogo.movimentarPeca(2, 1, 3, 1));
+		// movimento do peao preto
+		assertTrue(jogo.movimentarPeca(6, 0, 5, 0));
+		assertTrue(jogo.movimentarPeca(5, 0, 4, 0));
+		// movimento do cavalo preto
+		assertTrue(jogo.movimentarPeca(7, 1, 5, 2));
+		return jogo;
+	}
 
 	@Test
 	public void testeTabuleiroDoJogo() {
@@ -277,23 +295,7 @@ public class Teste {
 		assertEquals(jogo.getJogadorDaVez(), CorDaPeca.BRANCA);
 	}
 
-	public JogoXadrez cenario5() {
-		JogoXadrez jogo = new JogoXadrez();
-		// movimento do peao branca
-		assertTrue(jogo.movimentarPeca(1, 0, 2, 0));
-		assertTrue(jogo.movimentarPeca(2, 0, 3, 0));
-		// movimento da torre branca
-		assertTrue(jogo.movimentarPeca(0, 0, 1, 0));
-		assertTrue(jogo.movimentarPeca(1, 0, 2, 0));
-		assertTrue(jogo.movimentarPeca(2, 0, 2, 1));
-		assertTrue(jogo.movimentarPeca(2, 1, 3, 1));
-		// movimento do peao preto
-		assertTrue(jogo.movimentarPeca(6, 0, 5, 0));
-		assertTrue(jogo.movimentarPeca(5, 0, 4, 0));
-		// movimento do cavalo preto
-		assertTrue(jogo.movimentarPeca(7, 1, 5, 2));
-		return jogo;
-	}
+	
 
 	@Test
 	public void testePeaoComendoTorre() {
