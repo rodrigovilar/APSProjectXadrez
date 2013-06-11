6c17a2f70743f4f9d2dc2953e9957aae28b63944
hugomoreira <hugomoreira@dce.ufpb.br>
2013-06-11 00:51:29 -0300

    Adicionado Teste peao comendo cavalo e inclusão do cenário sete.

--- src/xadrez/Teste.java
+++ src/xadrez/Teste.java
@@ -74,6 +74,16 @@ public class Teste {
 		return jogo;
 
 	}
+	public JogoXadrez cenario7() {
+        JogoXadrez jogo = new JogoXadrez();
+        // movimento de um peao
+        assertTrue(jogo.movimentarPeca(1, 2, 2, 2));
+        // movimento do cavalo
+        assertTrue(jogo.movimentarPeca(7, 1, 5, 2));
+
+        return jogo;
+
+}
 	
 	@Test
 	public void testeTabuleiroDoJogo() {
@@ -343,7 +353,15 @@ public class Teste {
 		assertTrue(jogo.movimentarPeca(5, 2, 3, 1));
 		assertEquals(CorDaPeca.PRETA, jogo.corPeca(3, 1));
 	}
-	
+	@Test
+    public void testePeaoComendoCavalo() {
+            JogoXadrez jogo = this.cenario7();
+            assertTrue(jogo.movimentarPeca(1, 5, 2, 5));
+            assertTrue(jogo.movimentarPeca(5, 2, 3, 1));
+            assertTrue(jogo.movimentarPeca(2, 2, 3, 1));
+            assertEquals(CorDaPeca.BRANCA, jogo.corPeca(3, 1));
+
+    }	
 	
 	
 }
